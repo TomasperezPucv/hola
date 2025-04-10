8e2fb94f17a0ac186dc483b0ea6c1bd8ffd2c7e7
TomasperezPucv <127423299+TomasperezPucv@users.noreply.github.com>
2025-04-10 19:02:17 -0400

    tst ngif

--- src/app/pages/profiles/profiles.page.ts
+++ src/app/pages/profiles/profiles.page.ts
@@ -9,6 +9,7 @@ import { Usuario } from 'src/app/models/usuario';
 })
 export class ProfilesPage implements OnInit {
   usuarios: Usuario[] = [];
+  soloCasados: boolean = false;
   constructor() { }
 
   ngOnInit() {
@@ -18,9 +19,19 @@ export class ProfilesPage implements OnInit {
       { id: 11223344, nombre: "Pedro", casado: true, edad: 28, rango: "admin" },
       { id: 44332211, nombre: "Ana", casado: false, edad: 22, rango: "usuario" },
       { id: 55667788, nombre: "Luis", casado: true, edad: 35, rango: "admin" },
+      { id: 88776655, nombre: "Laura", casado: false, edad: 29, rango: "usuario" },
+      { id: 99887766, nombre: "Carlos", casado: true, edad: 32, rango: "admin" },
+      { id: 66778899, nombre: "Sofia", casado: false, edad: 27, rango: "usuario" },
+      { id: 33445566, nombre: "Javier", casado: true, edad: 31, rango: "admin" },
+      { id: 55664433, nombre: "Valeria", casado: false, edad: 24, rango: "usuario" }
     ];
 
 
+  }
+
+  filtrarCasados() {
+    this.soloCasados = !this.soloCasados;
+    console.log(this.soloCasados);
   }
 
 }
