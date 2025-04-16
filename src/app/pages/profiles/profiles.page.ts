fb6a937809dd43abc9d3bbdad45949e16335a8e5
TomasperezPucv <127423299+TomasperezPucv@users.noreply.github.com>
2025-04-16 17:34:43 -0400

    uso express de ion-grid header

--- src/app/pages/profiles/profiles.page.ts
+++ src/app/pages/profiles/profiles.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
-
+import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-profiles',
   templateUrl: './profiles.page.html',
@@ -10,23 +10,11 @@ import { Usuario } from 'src/app/models/usuario';
 export class ProfilesPage implements OnInit {
   usuarios: Usuario[] = [];
   soloCasados: boolean = false;
-  constructor() { }
+  constructor(private users:UsersService) { }
 
   ngOnInit() {
-    this.usuarios= [ 
-      { id: 12345678, nombre: "Juan", casado: true, edad: 25, rango: "admin" },
-      { id: 87654321, nombre: "Maria", casado: false, edad: 30, rango: "usuario" },
-      { id: 11223344, nombre: "Pedro", casado: true, edad: 28, rango: "admin" },
-      { id: 44332211, nombre: "Ana", casado: false, edad: 22, rango: "usuario" },
-      { id: 55667788, nombre: "Luis", casado: true, edad: 35, rango: "admin" },
-      { id: 88776655, nombre: "Laura", casado: false, edad: 29, rango: "usuario" },
-      { id: 99887766, nombre: "Carlos", casado: true, edad: 32, rango: "admin" },
-      { id: 66778899, nombre: "Sofia", casado: false, edad: 27, rango: "usuario" },
-      { id: 33445566, nombre: "Javier", casado: true, edad: 31, rango: "admin" },
-      { id: 55664433, nombre: "Valeria", casado: false, edad: 24, rango: "usuario" }
-    ];
-
-
+    this.usuarios = this.users.llenarUsuarios();
+    console.log(this.usuarios);
   }
 
   filtrarCasados() {
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private usuarios: Usuario[] = [];
+  
+  constructor() { }
+
+  llenarUsuarios(): Usuario[] {
+    this.usuarios= [ 
+      { id: 12345678, nombre: "Juan", casado: true, edad: 25, rango: "admin" },
+      { id: 87654321, nombre: "Maria", casado: false, edad: 30, rango: "usuario" },
+      { id: 11223344, nombre: "Pedro", casado: true, edad: 28, rango: "admin" },
+      { id: 44332211, nombre: "Ana", casado: false, edad: 22, rango: "usuario" },
+      { id: 55667788, nombre: "Luis", casado: true, edad: 35, rango: "admin" },
+      { id: 88776655, nombre: "Laura", casado: false, edad: 29, rango: "usuario" },
+      { id: 99887766, nombre: "Carlos", casado: true, edad: 32, rango: "admin" },
+      { id: 66778899, nombre: "Sofia", casado: false, edad: 27, rango: "usuario" },
+      { id: 33445566, nombre: "Javier", casado: true, edad: 31, rango: "admin" },
+      { id: 55664433, nombre: "Valeria", casado: false, edad: 24, rango: "usuario" }
+    ];
+
+    return this.usuarios;
+  }
+}
